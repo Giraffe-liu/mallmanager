62920d9993e171d29d07311a1c68cd0f38e49fc2
“g“Giraffe-liu <18202530619@163.com>
2018-11-02 20:14:48 +0800

    分页功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import moment from 'moment'
 import login from '@/views/login'
 import home from '@/views/home'
 import user from '@/views/user/user'
@@ -7,6 +8,9 @@ import http from '@/assets/plugins/http'
 
 Vue.use(Router)
 Vue.use(http)
+Vue.filter('fmDate', (value, fmString) => {
+  return moment(value).format(fmString)
+})
 
 export default new Router({
   routes: [
--- src/views/user/user.vue
+++ src/views/user/user.vue
@@ -16,48 +16,49 @@
         </el-col>
     </el-row>
     <!-- 表格 -->
-    <template>
-        <el-table :data="list" style="width: 100%">
-            <el-table-column type="index" label="#" width="80">
-            </el-table-column>
-            <el-table-column prop="username" label="姓名" width="100">
-            </el-table-column>
-            <el-table-column prop="email" label="邮箱" width="180">
-            </el-table-column>
-            <el-table-column prop="mobile" label="电话" width="180">
-            </el-table-column>
-            <el-table-column prop="create_time" label="创建日期" width="180">
-            </el-table-column>
-            <el-table-column label="用户状态">
-                <!-- 用户状态开关 -->
-                <template slot-scope="scope">
-                    <el-switch
-                    v-model="scope.row.mg_state"
-                    active-color="#13ce66"
-                    inactive-color="#ff4949">
-                    </el-switch>
-                </template>
-            </el-table-column>
-            <el-table-column label="操作">
-                <!-- 操作按钮 -->
-                <template slot-scope="scope">
-                    <el-button plain size="mini" type="primary" icon="el-icon-edit" circle></el-button>
-                    <el-button plain size="mini" type="danger" icon="el-icon-delete" circle></el-button>
-                    <el-button plain size="mini" type="success" icon="el-icon-check" circle></el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+    <el-table  v-loading="loading" :data="list" style="width: 100%">
+        <el-table-column type="index" label="#" width="80">
+        </el-table-column>
+        <el-table-column prop="username" label="姓名" width="100">
+        </el-table-column>
+        <el-table-column prop="email" label="邮箱" width="180">
+        </el-table-column>
+        <el-table-column prop="mobile" label="电话" width="180">
+        </el-table-column>
+        <el-table-column label="创建日期" width="180">
+            <template slot-scope="scope">
+                {{scope.row.create_time | fmDate('YYYY-MM-DD')}}
+            </template>
+        </el-table-column>
+        <el-table-column label="用户状态">
+            <!-- 用户状态开关 -->
+            <template slot-scope="scope">
+                <el-switch
+                v-model="scope.row.mg_state"
+                active-color="#13ce66"
+                inactive-color="#ff4949">
+                </el-switch>
+            </template>
+        </el-table-column>
+        <el-table-column label="操作">
+            <!-- 操作按钮 -->
+            <template slot-scope="scope">
+                <el-button plain size="mini" type="primary" icon="el-icon-edit" circle></el-button>
+                <el-button plain size="mini" type="danger" icon="el-icon-delete" circle></el-button>
+                <el-button plain size="mini" type="success" icon="el-icon-check" circle></el-button>
+            </template>
+        </el-table-column>
+    </el-table>
         <!-- 分页 -->
-        <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="pagenum"
-        :page-sizes="[100, 200, 300, 400]"
-        :page-size="pagesize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="total">
-        </el-pagination>
-    </template>
+    <el-pagination
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+    :current-page="pagenum"
+    :page-sizes="[2, 4, 6, 8]"
+    :page-size="pagesize"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="total">
+    </el-pagination>
 </el-card>
 </template>
 
@@ -65,35 +66,55 @@
 export default {
   data () {
     return {
+      // 获取用户数据的数组
       list: [],
+      // 分页相关属性
       pagenum: 1,
       pagesize: 2,
-      total: 0
+      total: 0,
+      // 加载动画属性
+      loading: true
     }
   },
   created () {
     this.loadData()
   },
   methods: {
+    // 处理当前展示的是第几页
     handleCurrentChange (val) {
-      console.log(val)
+      this.pagenum = val
+      this.loadData()
+      console.log(this.pagesize)
     },
+    // 处理每页有多少条
     handleSizeChange (val) {
-      console.log(val)
+      this.pagesize = val
+      this.loadData()
+      console.log(this.pagenum)
     },
+    // 页面加载完成
     async loadData () {
+      this.loading = true
       const res = await this.$http.get('users', {
         headers: {
           Authorization: window.sessionStorage.getItem('token')
         },
         params: {
-          pagenum: 1,
-          pagesize: 10
+          pagenum: this.pagenum,
+          pagesize: this.pagesize
         }
       })
-      const {data: {data: { users }}} = res
-      this.list = users
-      console.log(this.list)
+      this.loading = false
+      const data = res.data
+      const {meta: { msg, status }} = data
+      if (status === 200) {
+        const {data: {users, total}} = data
+        this.list = users
+        this.total = total
+        // console.log(this.pagesize, this.pagenum, this.total)
+      } else {
+        this.$message.error(msg)
+      }
     }
   }
 }
